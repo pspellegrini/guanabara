bc06a040b3d84cb1183236b5af304208232e3ffc
pspellegrini <pspellegrini@gmail.com>
2023-09-09 11:13:01 -0300

    tipos primitivos

--- Python3/Mundo01/tipos_primitivos_saída.py
+++ Python3/Mundo01/tipos_primitivos_saída.py
@@ -15,12 +15,30 @@ Textos Alfanuméricos ou Strings
     = 'Olá', '7.5', ''
 '''
 
+# Entrada
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite mais um número: '))
+n3 = float(input('Digite um número decimal: '))
+n4 = str(input('Digite uma frase: '))
+n5 = bool(input('Digite qualquer coisa: '))
+
+# Processamento
 s = n1 + n2
+
+
+# Saída
+print(type(n1))
+print(n4.isalnum())
+
 
 print('A soma vale', s)
 # ou
 print(f'A soma vale {s}.')
 # ou
 print('A soma vale {}.'.format(s))
+# ou
+print('A soma entre', n1, 'e', n2, 'vale', s)
+# ou
+print('A soma entre {} e {} vale {}.'.format(n1, n2, s))
+# ou
+print(f'A soma entre {n1} e {n2} vale {s}.')
